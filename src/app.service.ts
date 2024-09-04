f8c7b83df3211b69ed23e06da1c8a1a990ede81f
Pongpipat Siangsai <piigtryzlovemiku@gmail.com>
2024-09-04 15:58:12 +0700

    Sets up Api app Service and Controller.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import axios from 'axios';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AppService {
+  constructor(@Inject('MATH_SERVICE') private client: ClientProxy) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async sum1(numbers: number[]): Promise<number> {
+    try {
+      const response = await axios.post('http://localhost:3001/math/sum', {
+        numbers,
+      });
+      return response.data;
+    } catch (error) {
+      console.error('Error called the Microservie:', error);
+    }
+  }
+  sum2(numbers: number[]): Observable<number> {
+    const pattern = { cmd: 'sum' };
+    return this.client.send<number>(pattern, numbers);
   }
 }
